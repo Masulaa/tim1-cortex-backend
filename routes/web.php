d706794457ba3a8130e420213c9e0299a462abc0
Luka <lukamasulovic2@gmail.com>
2024-09-15 14:09:21 +0200

    installed admin lte3

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,18 @@
 <?php
 
+use App\Http\Middleware\IsAdmin;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/admin', [App\Http\Controllers\AdminController::class, 'index'])
+    ->middleware('auth', IsAdmin::class)->name('admin.dashboard');
+
+
+
+//     Route::get('/admin', [AdminController::class, 'index'])
+//    name('admin.dashboard');
