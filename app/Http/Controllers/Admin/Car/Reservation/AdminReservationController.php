0974def8895487902e6f2f7d877c8a5542674236
Luka <lukamasulovic2@gmail.com>
2024-10-14 03:09:29 +0200

    hotfix

--- app/Http/Controllers/Admin/Car/Reservation/AdminReservationController.php
+++ app/Http/Controllers/Admin/Car/Reservation/AdminReservationController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers\Admin\Car\Reservation;
 
-
 use App\Models\Car;
 use App\Models\Reservation;
 use App\Models\User;
@@ -27,6 +26,7 @@ class AdminReservationController extends Controller
         $reservations = Reservation::with(['car', 'user'])
             ->orderBy('created_at', 'desc')
             ->get();
+
         return view('admin.reservations.reservation_list', compact('reservations'));
     }
 
@@ -44,50 +44,12 @@ class AdminReservationController extends Controller
         return view('admin.reservations.reservation_edit', compact('reservation', 'cars', 'users'));
     }
 
-    public function cancel(Request $request, $id)
-    {
-        $reservation = Reservation::findOrFail($id);
-        $currentTime = now();
-        $startDate = $reservation->start_date;
-
-        // Primeniti pravilo o otkazu
-        if ($currentTime->diffInHours($startDate) < 48) {
-            $cancellationFee = $reservation->total_price * 0.50;
-            // Ovde možeš dodati logiku za obaveštenje korisnika o naknadi
-            return response()->json([
-                'status' => 'error',
-                'message' => 'Cancellation fee of 50% applies as the cancellation is within 48 hours of the start date.',
-                'cancellation_fee' => $cancellationFee
-            ], 403); // 403 Forbidden ili 422 Unprocessable Entity, zavisno od tvog pravila
-        }
-
-        $reservation->delete();
-
-        return response()->json([
-            'status' => 'success',
-            'message' => 'Reservation cancelled successfully'
-        ], 200); // 200 OK
-    }
-
-
     public function update(Request $request, $id)
     {
         $reservation = Reservation::findOrFail($id);
-        $currentTime = now();
-        $startDate = $reservation->start_date;
-
-        // Proveri da li je izmena rezervacije manje od 48 sati pre početka
-        if ($currentTime->diffInHours($startDate) < 48) {
-            // Primeniti politiku 50% naknade
-            $cancellationFee = $reservation->total_price * 0.50;
-            // Ovde možeš dodati logiku za obračun ili obaveštenje korisnika o naknadi
-        }
-
         $reservation->update($request->all());
-
         return redirect()->route('admin.reservations.index')->with('success', 'Reservation updated successfully');
     }
-
 
     public function accept($id)
     {
